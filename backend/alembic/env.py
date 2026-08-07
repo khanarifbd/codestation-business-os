a06b82826917b3d8fb79e459b353ed1d08ab66fd
Md Ariful Islam (khan arif) <76259979+khanarifbd@users.noreply.github.com>
2026-08-07 22:36:02 +0600

    company setup: include system defaults metadata

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -18,6 +18,7 @@ from app.models import (  # noqa: F401
     OrganizationLocalizationSettings,
     OrganizationOnlineProfile,
     OrganizationProfile,
+    OrganizationSystemDefaults,
     Subscription,
     User,
 )
