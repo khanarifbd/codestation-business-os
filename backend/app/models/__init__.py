12e7d6a1e7c0161fc6347a39f2e96f9020500aec
Md Ariful Islam (khan arif) <76259979+khanarifbd@users.noreply.github.com>
2026-08-11 01:58:21 +0600

    register investments and funding v2 models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,19 @@
 from app.models.accounting import JournalEntry, JournalLine, LedgerAccount
 from app.models.accounting_money import AccountingMoneyEntry
 from app.models.activity_log import ActivityLog
-from app.models.capital import CompanyInvestment, CompanyLoan, InvestmentReturn, InvestorPayout, LoanRepayment, ProjectInvestor
+from app.models.capital import (
+    CompanyInvestment,
+    CompanyInvestmentFunding,
+    CompanyInvestor,
+    CompanyInvestorFunding,
+    CompanyInvestorPayout,
+    CompanyLoan,
+    InvestmentReturn,
+    InvestorPayout,
+    LoanRepayment,
+    ProjectInvestor,
+    ProjectInvestorFunding,
+)
 from app.models.client_access import ClientMembership
 from app.models.company_defaults import OrganizationExchangeRate, OrganizationSystemDefaults
 from app.models.company_settings import (
@@ -56,13 +68,17 @@ from app.models.team import Department, Designation, Employee, EmployeeInvitatio
 from app.models.user import User
 
 __all__ = [
-    "AccountTransfer", "AccountingMoneyEntry", "AccountingPeriod", "ActivityLog", "AttendanceRecord", "Client", "ClientMembership", "CompanyInvestment", "CompanyLoan", "CustomerAdvance", "CustomerAdvanceApplication", "Department", "Designation", "Employee", "EmployeeHRDocument", "EmployeeInvitation", "EmployeeLifecycleEvent", "EmployeeShiftAssignment",
-    "Expense", "ExpenseCategory", "ExpenseDocument", "FinancialAccount", "FinancialTransaction", "HRAnnouncement", "HRAnnouncementAcknowledgement", "HRHoliday", "HRShift", "InvestmentReturn", "InvestorPayout", "Invoice", "InvoiceItem", "JobCandidate", "JobOpening", "JournalEntry", "JournalLine",
-    "Lead", "LeadInteraction", "LeadSource", "LeadStatus", "LeaveRequest", "LeaveType", "LedgerAccount", "LoanDisbursement", "LoanFee", "LoanRepayment", "LoanScheduleItem", "Membership", "Order", "OrderItem", "PayableBill", "PayablePayment", "Payment", "PerformanceReview", "PostingIdempotency", "ProjectInvestor",
-    "Organization", "OrganizationAddress", "OrganizationBranding", "OrganizationDocument", "OrganizationExchangeRate",
-    "OrganizationDocumentSequence", "OrganizationFinancialSettings", "OrganizationIdentifier",
-    "OrganizationLocalizationSettings", "OrganizationOnlineProfile", "OrganizationProfile",
+    "AccountTransfer", "AccountingMoneyEntry", "AccountingPeriod", "ActivityLog", "AttendanceRecord", "Client", "ClientMembership",
+    "CompanyInvestment", "CompanyInvestmentFunding", "CompanyInvestor", "CompanyInvestorFunding", "CompanyInvestorPayout", "CompanyLoan",
+    "CustomerAdvance", "CustomerAdvanceApplication", "Department", "Designation", "Employee", "EmployeeHRDocument", "EmployeeInvitation",
+    "EmployeeLifecycleEvent", "EmployeeShiftAssignment", "Expense", "ExpenseCategory", "ExpenseDocument", "FinancialAccount", "FinancialTransaction",
+    "HRAnnouncement", "HRAnnouncementAcknowledgement", "HRHoliday", "HRShift", "InvestmentReturn", "InvestorPayout", "Invoice", "InvoiceItem",
+    "JobCandidate", "JobOpening", "JournalEntry", "JournalLine", "Lead", "LeadInteraction", "LeadSource", "LeadStatus", "LeaveRequest", "LeaveType",
+    "LedgerAccount", "LoanDisbursement", "LoanFee", "LoanRepayment", "LoanScheduleItem", "Membership", "Order", "OrderItem", "PayableBill",
+    "PayablePayment", "Payment", "PerformanceReview", "PostingIdempotency", "ProjectInvestor", "ProjectInvestorFunding", "Organization",
+    "OrganizationAddress", "OrganizationBranding", "OrganizationDocument", "OrganizationExchangeRate", "OrganizationDocumentSequence",
+    "OrganizationFinancialSettings", "OrganizationIdentifier", "OrganizationLocalizationSettings", "OrganizationOnlineProfile", "OrganizationProfile",
     "OrganizationRole", "OrganizationSystemDefaults", "PayrollEntry", "PayrollPeriod", "PayrollRun", "SalaryProfile", "Project", "ProjectCredential",
-    "ProjectDocument", "ProjectMember", "ProjectMilestone", "ProjectTask", "ProjectWorkLog", "RecurringExpense",
-    "Quotation", "QuotationItem", "Subscription", "User", "Vendor",
+    "ProjectDocument", "ProjectMember", "ProjectMilestone", "ProjectTask", "ProjectWorkLog", "RecurringExpense", "Quotation", "QuotationItem",
+    "Subscription", "User", "Vendor",
 ]
